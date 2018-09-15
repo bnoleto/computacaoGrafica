70511ad989a5cf3053bb2d33ae1323e284ff75d5
bnoleto <bnoleto@users.noreply.github.com>
2018-09-15 15:37:10 -0300

    Atividade 5.2 completa.

--- Atividade5.2/src/Atividade5.2.cpp
+++ Atividade5.2/src/Atividade5.2.cpp
@@ -3,13 +3,13 @@
 #include "Teardrop.h"
 #include <iostream>
 #include <fstream>
-#include <Math.h>
+#include <math.h>
 
 using namespace std;
 
 class Funcoes{
 	public:
-	Teardrop* teardrop = nullptr;
+	vector<Teardrop*>* lista_teardrop = new vector<Teardrop*>;
 
 	void config_glut(){
 		glutCreateWindow("Atividade 5.2"); // Create a window with the given title
@@ -18,6 +18,46 @@ class Funcoes{
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set background color to black and opaque
 
 		glColor3ub(255, 0, 0);
+	}
+
+	float rad(float angulo){	// converterá de graus para radianos
+		return angulo*3.141592/180;
+	}
+
+	void novo_cjto_teardrop(int tipo, Ponto* ponto_central){
+
+		lista_teardrop->clear();
+
+		if(tipo == 1){
+			int x = ponto_central->get_x();
+			int y = ponto_central->get_y();
+			for(int i = 0; i<8; i++){
+
+				float angulo = i*45;
+
+				Ponto* p_inicial_teardrop = new Ponto(
+						x + 30*cos(rad(angulo)),
+						y - 30*sin(rad(angulo))
+				);
+
+				lista_teardrop->push_back(new Teardrop(p_inicial_teardrop,30,angulo+90));
+			}
+		}
+		else{
+			int x = ponto_central->get_x();
+			int y = ponto_central->get_y();
+			for(int i = 0; i<8; i++){
+
+				float angulo = i*45;
+
+				Ponto* p_inicial_teardrop = new Ponto(
+						x + cos(rad(angulo)),
+						y - sin(rad(angulo))
+				);
+
+				lista_teardrop->push_back(new Teardrop(p_inicial_teardrop,30,angulo));
+			}
+		}
 	}
 
 };
@@ -30,8 +70,8 @@ void display() {
 
 	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
 
-	Teardrop* atual = funcoes.teardrop;
-	if(funcoes.teardrop != nullptr){
+	for(unsigned int j = 0; j< funcoes.lista_teardrop->size(); j++){
+		Teardrop* atual = funcoes.lista_teardrop->at(j);
 		glBegin(GL_LINE_LOOP);
 			for(unsigned int i = 0; i< atual->get_pontos()->size(); i++){
 				glVertex2i(atual->get_pontos()->at(i)->get_x(),atual->get_pontos()->at(i)->get_y());
@@ -46,7 +86,11 @@ void mouse(int botao, int estado, int x, int y){
 
 	if (estado == GLUT_DOWN){
 		if(botao == GLUT_LEFT_BUTTON){
-			funcoes.teardrop = new Teardrop(new Ponto(x,y),30,120);
+			funcoes.novo_cjto_teardrop(0, new Ponto(x,y));
+
+		}
+		if(botao == GLUT_RIGHT_BUTTON){
+			funcoes.novo_cjto_teardrop(1, new Ponto(x,y));
 		}
 	}
 }
